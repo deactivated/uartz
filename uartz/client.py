de8e280c69d161f3e433c0283bf672c7b44d29d5
mike spindel <mike@spindel.is>
2013-11-04 21:28:10 -0800

    add UartzStream client helper

--- uartz/client.py
+++ uartz/client.py
@@ -1,11 +1,18 @@
-import zmq
 import base64
+from collections import namedtuple
+
+import zmq
+from zmq.eventloop.zmqstream import ZMQStream
+
+
+UartzMsg = namedtuple("UartzMsg", "dev msg")
 
 
 class UartzClient(object):
 
     def __init__(self, addr):
         self.addr = addr
+        self.sock = None
         self.reset_sock()
 
     def reset_sock(self):
@@ -26,3 +33,31 @@ class UartzClient(object):
     def write_uart(self, path, data):
         encoded = base64.b64encode(data).decode('ascii')
         return self._do_command("WRITE", path=path, data=encoded)
+
+
+class UartzStream(object):
+
+    def __init__(self, addr, io_loop=None):
+        self.addr = addr
+        self.ioloop = io_loop
+
+        self.stream = None
+        self.reset_stream()
+
+    def reset_stream(self):
+        ctx = zmq.Context.instance()
+        sock = ctx.socket(zmq.SUB)
+        sock.connect(self.addr)
+        sock.setsockopt(zmq.SUBSCRIBE, b'')
+
+        self.stream = ZMQStream(sock, self.ioloop)
+        self.stream.on_recv(self._handle_msg)
+
+    def _handle_msg(self, msg):
+        assert len(msg) == 1
+        msg = msg[0]
+        chan_idx = msg.index(b":")
+
+        assert chan_idx > 0
+        self.handle_msg(UartzMsg(dev=msg[:chan_idx],
+                                 msg=msg[chan_idx + 1:]))
